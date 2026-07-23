754eb8f303122874e79da6250a89a306b089fe24
Samukelisiwe2 <ST10493347@rcconnect.edu.za>
2026-07-23 16:03:19 +0000

    Create the dashboard Controller

---
+++ CyberWatch_web/Controllers/DashboardController.cs
@@ -0,0 +1,24 @@
+﻿using CyberWatch_web.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CyberWatch_web.Controllers;
+
+public class DashboardController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public DashboardController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var alerts = await _context.SecurityAlerts
+            .OrderByDescending(a => a.CreatedAtUtc)
+            .ToListAsync();
+
+        return View(alerts);
+    }
+}
