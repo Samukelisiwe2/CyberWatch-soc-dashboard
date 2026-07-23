0bb1774112d181bb1537c08b0fa5937cfccfa8b1
Samukelisiwe2 <ST10493347@rcconnect.edu.za>
2026-07-23 18:36:43 +0000

    Dashboard veiw model

---
+++ CyberWatch_web/Models/DashboardViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace CyberWatch_web.Models;
+
+public class DashboardViewModel
+{
+    public int TotalAlerts { get; set; }
+
+    public int OpenAlerts { get; set; }
+
+    public int HighSeverityAlerts { get; set; }
+
+    public int TotalSecurityLogs { get; set; }
+
+    public List<SecurityAlert> RecentAlerts { get; set; } = [];
+}
